1592f95e2bddc57fb6bc77fc6df8a91f073d20de
user1565413124 <1565413124@qq.com>
2018-05-15 08:42:08 +0800

    Add

---
+++ JavaWeb06/my.js
@@ -0,0 +1,23 @@
+function fn3(){
+	console.log("我是文件调用式");
+}
+
+<div class="down_4">
+	<h3>评论 <span>共0条评论</span></h3>
+</div>
+<div class="down_5">
+		<a id="c1">期待你的神评论.....</a>
+		<span>剩余<a id="c2">300</a>字</span>
+	</div>
+	<div class="down_6">
+		<input type="button" value="发表评论" style="cursor: pointer;">
+	</div>
+
+
+<div class="right">
+	<h3>简介</h3>
+	<div class="right_1">
+		<span>萌徳联手P&B新手Khalid带来新歌《Youth》;肖敬滕全新单曲《让我为你唱情歌》清纯上线，心爱的姑娘，让我对你说爱你;周深个人首次巡演拉开，宣传曲《花开》上线；戳爷新</span>
+		<p>[更多]</p>
+	</div>
+</div>
